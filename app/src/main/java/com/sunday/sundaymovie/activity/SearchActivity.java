a7eb0c7a369a4aa58cc63b88b169d065c5b63b39
agentchen <agentchen97@gmail.com>
2017-07-28 10:43:02 +0800

    优化搜索体验

--- app/src/main/java/com/sunday/sundaymovie/activity/SearchActivity.java
+++ app/src/main/java/com/sunday/sundaymovie/activity/SearchActivity.java
@@ -7,16 +7,21 @@ import android.content.SharedPreferences;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,7 +31,7 @@ import android.widget.Toast;
 
 import com.sunday.sundaymovie.R;
 import com.sunday.sundaymovie.adapter.SearchAdapter;
-import com.sunday.sundaymovie.model.SearchResult;
+import com.sunday.sundaymovie.bean.SearchResult;
 import com.sunday.sundaymovie.net.Api;
 import com.sunday.sundaymovie.net.OkManager;
 import com.sunday.sundaymovie.net.callback.SearchCallBack;
@@ -36,45 +41,45 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class SearchActivity extends BaseActivity {
-    int activityCloseExitAnimation;
+public class SearchActivity extends BaseActivity implements Animation.AnimationListener {
+    int mActivityCloseExitAnimation;
     private OkManager mOkManager;
-    private String query;
+    private String mQuery;
     private SearchView mSearchView;
     private ListView mListViewHistory;
     private ArrayAdapter<String> mHistoryAdapter;
-    private TextView mTextView, mTVSearchNull;
+    private SearchAdapter mSearchAdapter;
+    private TextView mTVSearchNull;
     private RecyclerView mRecyclerView;
     private SearchResult mSearchResult;
     private ProgressBar mProgressBar;
-    private boolean isSearching = false;
+    private CardView mCardViewHistory;
+    private View mViewBGSearch;
+    private boolean mIsSearching = false;
+    private boolean mIsFirstSearch = true;
+    private TranslateAnimation mAnimShowCard;
+    private AlphaAnimation mAnimShowBG;
+    private TranslateAnimation mAnimHideCard;
+    private AlphaAnimation mAnimHideBG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final List<String> history = getSearchHistory();
-        mHistoryAdapter = new ArrayAdapter<>(this, R.layout.item_search_history, history);
-        mListViewHistory.setAdapter(mHistoryAdapter);
         mOkManager = OkManager.getInstance();
-        mTextView = (TextView) LayoutInflater.from(this).inflate(R.layout.clean_search_history, mListViewHistory, false);
-
-        mTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mHistoryAdapter.clear();
-                mListViewHistory.removeFooterView(mTextView);
-                mHistoryAdapter.notifyDataSetChanged();
-                cleanSearchHistory();
-            }
-        });
-        if (mHistoryAdapter.getCount() != 0) {
-            mListViewHistory.addFooterView(mTextView);
-        }
+        TextView TVClean = (TextView) LayoutInflater.from(this).inflate(R.layout.clean_search_history, mListViewHistory, false);
+        mListViewHistory.addFooterView(TVClean);
+        mHistoryAdapter = new ArrayAdapter<>(this, R.layout.item_search_history);
+        mListViewHistory.setAdapter(mHistoryAdapter);
         mListViewHistory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position < history.size()) {
-                    mSearchView.setQuery(history.get(position), true);
+                if (position < mHistoryAdapter.getCount()) {
+                    mSearchView.setQuery(mHistoryAdapter.getItem(position), true);
+                } else {
+                    hideHistory();
+                    mHistoryAdapter.clear();
+                    mHistoryAdapter.notifyDataSetChanged();
+                    cleanSearchHistory();
                 }
             }
         });
@@ -92,15 +97,45 @@ public class SearchActivity extends BaseActivity {
                 return true;
             }
         });
-        if (!"".equals(query)) {
-            mSearchView.setQuery(query, true);
+        mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    if (mHistoryAdapter.getCount() != 0) {
+                        mHistoryAdapter.clear();
+                    }
+                    mHistoryAdapter.addAll(getSearchHistory());
+                    mHistoryAdapter.notifyDataSetChanged();
+                    if (mHistoryAdapter.getCount() > 0) {
+                        showHistory();
+                    } else {
+                        if (mCardViewHistory.getVisibility() == View.VISIBLE) {
+                            mCardViewHistory.setVisibility(View.GONE);
+                        }
+                        if (mIsFirstSearch) {
+                            mIsFirstSearch = false;
+                        }
+                    }
+                } else {
+                    hideHistory();
+                }
+            }
+        });
+        mViewBGSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSearchView.clearFocus();
+            }
+        });
+        if (!"".equals(mQuery)) {
+            mSearchView.setQuery(mQuery, true);
         }
     }
 
     @Override
     protected void initParams(Bundle bundle) {
         if (bundle != null && bundle.containsKey(SearchManager.QUERY)) {
-            query = bundle.getString(SearchManager.QUERY);
+            mQuery = bundle.getString(SearchManager.QUERY);
         }
 //        使activity退出anim生效
         TypedArray activityStyle = getTheme().obtainStyledAttributes(
@@ -110,7 +145,7 @@ public class SearchActivity extends BaseActivity {
         activityStyle = getTheme().obtainStyledAttributes(windowAnimationStyleResId,
                 new int[]{android.R.attr.activityCloseEnterAnimation,
                         android.R.attr.activityCloseExitAnimation});
-        activityCloseExitAnimation = activityStyle.getResourceId(1, 0);
+        mActivityCloseExitAnimation = activityStyle.getResourceId(1, 0);
         activityStyle.recycle();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
@@ -129,6 +164,8 @@ public class SearchActivity extends BaseActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_search);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mCardViewHistory = (CardView) findViewById(R.id.cv_search_parent);
+        mViewBGSearch = findViewById(R.id.bg_search);
     }
 
     @Override
@@ -144,34 +181,39 @@ public class SearchActivity extends BaseActivity {
     }
 
     private void doMySearch(String query) {
-        if (!isSearching) {
-            isSearching = true;
+        if (!mIsSearching) {
+            mIsSearching = true;
             mProgressBar.setVisibility(View.VISIBLE);
             mOkManager.asyncGet(Api.getSearchUrl(query), new SearchCallBack() {
                 @Override
                 public void onResponse(SearchResult response) {
-                    mProgressBar.setVisibility(View.GONE);
-                    mSearchResult = response;
-                    if (mSearchResult != null) {
+                    if (response != null) {
+                        mProgressBar.setVisibility(View.GONE);
+                        mSearchResult = response;
                         modelToView();
+                        mIsSearching = false;
+                    } else {
+                        onError();
                     }
-                    mListViewHistory.setVisibility(View.GONE);
-                    isSearching = false;
                 }
 
                 @Override
-                public void onError(Exception e) {
+                public void onError() {
                     mProgressBar.setVisibility(View.GONE);
-                    Toast.makeText(SearchActivity.this, "网络异常", Toast.LENGTH_SHORT).show();
-                    isSearching = false;
+                    Toast.makeText(SearchActivity.this, "有点问题", Toast.LENGTH_SHORT).show();
+                    mIsSearching = false;
                 }
             });
         }
     }
 
     private void modelToView() {
-        mRecyclerView.setAdapter(
-                new SearchAdapter(this, mSearchResult.getList(), mSearchResult.getType()));
+        if (mSearchAdapter == null) {
+            mSearchAdapter = new SearchAdapter(this, mSearchResult.getList(), mSearchResult.getType());
+        } else {
+            mSearchAdapter.replaceData(mSearchResult.getList(), mSearchResult.getType());
+        }
+        mRecyclerView.setAdapter(mSearchAdapter);
         if (mSearchResult.getList().size() == 0) {
             if (mTVSearchNull == null) {
                 mTVSearchNull = (TextView) findViewById(R.id.tv_search_null);
@@ -180,6 +222,48 @@ public class SearchActivity extends BaseActivity {
         } else if (mTVSearchNull != null) {
             mTVSearchNull.setVisibility(View.GONE);
         }
+    }
+
+    private void showHistory() {
+        if (mIsFirstSearch) {
+            mCardViewHistory.setVisibility(View.VISIBLE);
+            mViewBGSearch.setVisibility(View.VISIBLE);
+            mIsFirstSearch = false;
+            return;
+        }
+        mAnimShowCard = new TranslateAnimation(0, 0, -mCardViewHistory.getHeight(), 0);
+        mAnimShowCard.setDuration(300L);
+        mAnimShowCard.setInterpolator(new DecelerateInterpolator());
+        mAnimShowCard.setAnimationListener(this);
+
+        if (mAnimShowBG == null) {
+            mAnimShowBG = new AlphaAnimation(0, 1);
+            mAnimShowBG.setDuration(300L);
+            mAnimShowBG.setInterpolator(new DecelerateInterpolator());
+            mAnimShowBG.setAnimationListener(this);
+        }
+
+        mCardViewHistory.startAnimation(mAnimShowCard);
+        mViewBGSearch.startAnimation(mAnimShowBG);
+    }
+
+    private void hideHistory() {
+        if (mCardViewHistory.getVisibility() == View.GONE) {
+            return;
+        }
+        mAnimHideCard = new TranslateAnimation(0, 0, 0, -mCardViewHistory.getHeight());
+        mAnimHideCard.setInterpolator(new AccelerateInterpolator());
+        mAnimHideCard.setDuration(300L);
+        mAnimHideCard.setAnimationListener(this);
+
+        if (mAnimHideBG == null) {
+            mAnimHideBG = new AlphaAnimation(1, 0);
+            mAnimHideBG.setDuration(300L);
+            mAnimHideBG.setInterpolator(new AccelerateInterpolator());
+            mAnimHideBG.setAnimationListener(this);
+        }
+        mCardViewHistory.startAnimation(mAnimHideCard);
+        mViewBGSearch.startAnimation(mAnimHideBG);
     }
 
     public static void startMe(Context context, String query) {
@@ -192,7 +276,7 @@ public class SearchActivity extends BaseActivity {
 
     @Override
     public void finish() {
-        overridePendingTransition(0, activityCloseExitAnimation);
+        overridePendingTransition(0, mActivityCloseExitAnimation);
         super.finish();
     }
 
@@ -247,4 +331,26 @@ public class SearchActivity extends BaseActivity {
         return result;
     }
 
+    @Override
+    public void onAnimationStart(Animation animation) {
+        if (animation == mAnimShowCard) {
+            mCardViewHistory.setVisibility(View.VISIBLE);
+        } else if (animation == mAnimShowBG) {
+            mViewBGSearch.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if (animation == mAnimHideCard) {
+            mCardViewHistory.setVisibility(View.GONE);
+        } else if (animation == mAnimHideBG) {
+            mViewBGSearch.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
 }
